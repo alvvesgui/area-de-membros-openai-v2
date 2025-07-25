daa349891052b282523a1cb200e652745bdee407
Guilherme Alves <138711041+alvvesgui@users.noreply.github.com>
2025-07-25 19:30:54 -0300

    🔐 Backend atualizado com login e Google OAuth funcionando

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,18 @@
 // src/app.module.ts
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config'; // IMPORTA ConfigModule
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { ConversationModule } from './conversation/conversation.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, PrismaModule, ConversationModule],
-  
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // adiciona isso aqui, global para toda a aplicação
+    UsersModule,
+    AuthModule,
+    PrismaModule,
+    ConversationModule,
+  ],
 })
 export class AppModule {}
